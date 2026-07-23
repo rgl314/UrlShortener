147cddb3dd613807169c24e8c8a0768e70f68dae
iamraguls <raguls4477@gmail.com>
2026-07-23 09:11:49 +0530

    added redis configuration

---
+++ backend/src/main/java/com/ragul/UrlShortener/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package com.ragul.UrlShortener.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.ObjectMapper;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory connectionFactory,
+            ObjectMapper objectMapper
+    ){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        GenericJacksonJsonRedisSerializer genericJacksonJsonRedisSerializer =
+                new GenericJacksonJsonRedisSerializer(objectMapper);
+
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+
+        redisTemplate.setValueSerializer(genericJacksonJsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(genericJacksonJsonRedisSerializer);
+
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+}
