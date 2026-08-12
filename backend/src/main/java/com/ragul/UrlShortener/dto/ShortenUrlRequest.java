def64d7e2496e651cd4bb90179040366f29b1dfb
iamraguls <raguls4477@gmail.com>
2026-08-12 10:06:44 +0530

    removed size validation in url request dto

--- backend/src/main/java/com/ragul/UrlShortener/dto/ShortenUrlRequest.java
+++ backend/src/main/java/com/ragul/UrlShortener/dto/ShortenUrlRequest.java
@@ -3,7 +3,6 @@ package com.ragul.UrlShortener.dto;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
@@ -12,7 +11,6 @@ public record ShortenUrlRequest(
         @Pattern(regexp = "^https?://.+", message = "URL must start with http:// or https://")
         String originalUrl,
 
-        @Size(min = 5, max = 30)
         @Pattern(regexp = "^[a-zA-Z0-9_-]+$")
         String customAlias,
 
